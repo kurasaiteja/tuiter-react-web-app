de496ea39e1eeb29a445ec006eea51742853b8fd
Saiteja Kura <saitejakura@Saitejas-MacBook-Air.local>
2022-09-07 15:09:05 -0400

    third-commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to web-dev by Teja Kura.
+          Welcome to Web Development - Fall 2022
         </p>
         <a
           className="App-link"
