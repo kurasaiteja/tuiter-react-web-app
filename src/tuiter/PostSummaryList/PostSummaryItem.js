06b1621f4c28ec109c0ac8bec5c23b47ce68dbe6
Saiteja Kura <kurasaiteja@gmail.com>
2022-10-20 21:54:31 -0400

    A6 Fall 2022

--- src/tuiter/PostSummaryList/PostSummaryItem.js
+++ src/tuiter/PostSummaryList/PostSummaryItem.js
@@ -11,7 +11,7 @@ const PostSummaryItem = ({post}) => {
                 </div>
                 <div className="m-xxl-0 col-xxl-2 col-xl-2 offset-xl-2 col-lg-2  offset-lg-2 col-md-2 offset-md-2 col-sm-3 offset-sm-1">
                     <div className="card" style={{width: "5em", marginTop:"4px", marginBottom:"4px"}}>
-                        <img src={post.image} className="card-img-top" />
+                        <img src={post.image} alt="img" className="card-img-top" />
                     </div>
                 </div>
             </div>
