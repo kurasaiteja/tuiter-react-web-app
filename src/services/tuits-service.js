ed95b1975dc28a936df6df8ff260475701a2878c
Saiteja Kura <kurasaiteja@gmail.com>
2022-11-20 21:54:32 -0500

    A8 Fall 2022

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const API_BASE = process.env.REACT_APP_API_BASE || "https:localhost:4000/api";
-const TUITS_API = `${API_BASE}/tuits`;
+const API_BASE = process.env.REACT_APP_API_BASE || "https:localhost:4000/api/";
+const TUITS_API = `${API_BASE}tuits`;
 console.log(TUITS_API);
 
 export const createTuit = async (tuit) => {
